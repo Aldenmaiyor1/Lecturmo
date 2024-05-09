0648285859b4f3b643920c90755101110a0b7e74
tonexie <tone.xie@gmail.com>
2024-05-09 16:21:13 +1200

    update frontend host string

--- frontend/src/pages/LecturerPage/LecturerPage.jsx
+++ frontend/src/pages/LecturerPage/LecturerPage.jsx
@@ -31,6 +31,10 @@ const BASE_URL =
   import.meta.env.VITE_BACKEND_EXPRESS_APP_ENDPOINT_API_URL ??
   'http://localhost:3000/api';
 
+const FRONTEND_HOST_URL =
+  import.meta.env.VITE_FRONTEND_NETLIFY_APP_URL ??
+  'http://localhost:5173';
+
 const LecturerPage = () => {
   const { user } = useContext(AuthContext);
   if (user === null) {
@@ -313,7 +317,7 @@ const LecturerPage = () => {
                 color="primary"
                 onClick={() => {
                   window.open(
-                    `http://localhost:5173/qr?lecture=${selectedLectureId}&course=${selectedCourseId}&courseCode=${encodeURI(selectedCourseCode)}`,
+                    `${FRONTEND_HOST_URL}/qr?lecture=${selectedLectureId}&course=${selectedCourseId}&courseCode=${encodeURI(selectedCourseCode)}`,
                     '_blank'
                   );
                 }}
@@ -429,9 +433,9 @@ const LecturerPage = () => {
                             sx={
                               lecture._id == selectedLectureId
                                 ? {
-                                    backgroundColor: 'yellow',
-                                    cursor: 'pointer',
-                                  }
+                                  backgroundColor: 'yellow',
+                                  cursor: 'pointer',
+                                }
                                 : { cursor: 'pointer' }
                             }
                             onClick={() => {
